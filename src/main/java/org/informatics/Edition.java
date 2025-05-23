3bfabb39cce51f4d2d2bb86fedc74d5d94fd7104
Angelina <angelina.ivaylova.ivanova@gmail.com>
2025-05-23 15:01:54 +0300

    Add papertype and papersize in Edition

--- src/main/java/org/informatics/Edition.java
+++ src/main/java/org/informatics/Edition.java
@@ -1,17 +1,23 @@
 package org.informatics;
-
+// NOT PUSHED
 public class Edition {
     private String name;
     private int numberOfPages;
     private boolean isColor;
+    private PaperType paperType;
+    private PaperSize paperSize;
 
-    public Edition(String name, int numberOfPages, boolean isColor) {
+    public Edition(String name, int numberOfPages, boolean isColor, PaperType paperType, PaperSize paperSize) {
         this.name = name;
         this.numberOfPages = numberOfPages;
         this.isColor = isColor;
+        this.paperType = paperType;
+        this.paperSize = paperSize;
     }
 
     public String getName() { return name; }
     public int getNumberOfPages() { return numberOfPages; }
     public boolean isColor() { return isColor; }
+    public PaperType getPaperType() { return paperType; }
+    public PaperSize getPaperSize() { return paperSize; }
 }
