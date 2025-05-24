8100efa55a7f1eecc24d894bce8ab4dfe30375ed
Angelina <angelina.ivaylova.ivanova@gmail.com>
2025-05-24 19:03:47 +0300

    New fileReport class

---
+++ src/main/java/org/informatics/FileReport.java
@@ -0,0 +1,34 @@
+package org.informatics;
+// NOT PUSHED
+import org.informatics.employee.Employee;
+
+import java.io.*;
+import java.util.List;
+
+public class FileReport {
+
+    public static void saveReport(List<Edition> editions, double revenue, double expenses, String filename) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (Edition e : editions) {
+                writer.write(String.format("Edition: %s, Pages: %d, Color: %b, Paper: %s %s",
+                        e.getTitle(), e.getNumberOfPages(), e.isColor(), e.getPaperSize(), e.getPaperType()));
+                writer.newLine();
+            }
+            writer.write("Revenue: " + revenue);
+            writer.newLine();
+            writer.write("Expenses: " + expenses);
+        }
+    }
+
+    public static void serializeEmployee(Employee employee, String filename) throws IOException {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
+            out.writeObject(employee);
+        }
+    }
+
+    public static Employee deserializeEmployee(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+            return (Employee) in.readObject();
+        }
+    }
+}
