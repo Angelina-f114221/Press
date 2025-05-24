ea243bb9ad723b9f99765e9caa42e8859595324a
Angelina <angelina.ivaylova.ivanova@gmail.com>
2025-05-24 22:36:22 +0300

    BigDecimal to PrintingHouse class

--- src/main/java/org/informatics/PrintingHouse.java
+++ src/main/java/org/informatics/PrintingHouse.java
@@ -2,33 +2,36 @@ package org.informatics;
 
 import org.informatics.employee.Employee;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class PrintingHouse {
 
-    public static double calculateSalaryExpenses(Employee[] employees, double totalRevenue) {
-        double total = 0;
+    public static BigDecimal calculateSalaryExpenses(List<Employee> employees, BigDecimal totalRevenue) {
+        BigDecimal total = BigDecimal.ZERO;
         for (Employee employee : employees) {
-            total += employee.getSalary(totalRevenue);
+            total = total.add(employee.getSalary(totalRevenue));
         }
         return total;
     }
 
-    public static double calculatePaperExpenses(List<Paper> papers, List<Integer> quantities) {
-        double total = 0;
+    public static BigDecimal calculatePaperExpenses(List<Paper> papers, List<Integer> quantities) {
+        BigDecimal total = BigDecimal.ZERO;
         for (int i = 0; i < papers.size(); i++) {
-            total += papers.get(i).getActualPrice() * quantities.get(i);
+            BigDecimal cost = papers.get(i).getActualPrice().multiply(BigDecimal.valueOf(quantities.get(i)));
+            total = total.add(cost);
         }
         return total;
     }
 
-    public static double calculatePrintingRevenue(int numberOfCopies, double pricePerCopy,
-                                                  int discountThreshold, double discountPercent) {
+    public static BigDecimal calculatePrintingRevenue(int numberOfCopies, BigDecimal pricePerCopy,
+                                                      int discountThreshold, BigDecimal discountPercent) {
         if (numberOfCopies <= discountThreshold) {
-            return numberOfCopies * pricePerCopy;
+            return pricePerCopy.multiply(BigDecimal.valueOf(numberOfCopies));
         } else {
-            double discountedPrice = pricePerCopy * (1 - discountPercent / 100.0);
-            return numberOfCopies * discountedPrice;
+            BigDecimal discount = BigDecimal.ONE.subtract(discountPercent.divide(BigDecimal.valueOf(100)));
+            BigDecimal discountedPrice = pricePerCopy.multiply(discount);
+            return discountedPrice.multiply(BigDecimal.valueOf(numberOfCopies));
         }
     }
 }
