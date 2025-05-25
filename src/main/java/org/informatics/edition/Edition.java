32190e43bf07a2edb797f8d4dd6e3f18e1b7d6b7
Angelina <angelina.ivaylova.ivanova@gmail.com>
2025-05-25 21:45:33 +0300

    New Edition class update

--- src/main/java/org/informatics/edition/Edition.java
+++ src/main/java/org/informatics/edition/Edition.java
@@ -1,20 +1,35 @@
 package org.informatics.edition;
-
+import java.math.BigDecimal;
 import org.informatics.PaperSize;
 import org.informatics.PaperType;
+import org.informatics.PrintingHouseInstance;
 
 public class Edition {
     private String title;
     private int copies;
     private PaperSize size;
     private PaperType type;
+    private int numberOfPages;
+    private boolean isColor;
 
-    public Edition(String title, int copies, PaperSize size, PaperType type) {
+    public Edition(String title, int numberOfPages, boolean isColor, PaperType type, PaperSize size) {
         this.title = title;
-        this.copies = copies;
-        this.size = size;
+        this.numberOfPages = numberOfPages;
+        this.isColor = isColor;
         this.type = type;
+        this.size = size;
+        this.copies = 0;
     }
+
+    public BigDecimal calculatePrice(PrintingHouseInstance house) {
+        BigDecimal pricePerPage = house.getPaperPrice(type, size);
+        BigDecimal basePrice = pricePerPage.multiply(BigDecimal.valueOf(numberOfPages));
+        if (isColor) {
+            basePrice = basePrice.multiply(BigDecimal.valueOf(1.5)); // примерно 50% надценка за цвят
+        }
+        return basePrice.multiply(BigDecimal.valueOf(copies));
+    }
+
 
     public String getTitle() {
         return title;
@@ -24,11 +39,33 @@ public class Edition {
         return copies;
     }
 
-    public PaperSize getSize() {
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public PaperSize getPaperSize() {
         return size;
     }
 
-    public PaperType getType() {
+    public PaperType getPaperType() {
         return type;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public boolean isColor() {
+        return isColor;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s [title=%s, copies=%d, pages=%d, color=%b, paperType=%s, paperSize=%s]",
+                getEditionType(), title, copies, numberOfPages, isColor, type, size);
+    }
+
+    public String getEditionType() {
+        return "Edition";
     }
 }
