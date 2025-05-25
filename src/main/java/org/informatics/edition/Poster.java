ffc65a650ea4cead6d2c46a6fc359b76cabf7cb1
Angelina <angelina.ivaylova.ivanova@gmail.com>
2025-05-25 17:17:09 +0300

    New Poster class

---
+++ src/main/java/org/informatics/edition/Poster.java
@@ -0,0 +1,14 @@
+package org.informatics.edition;
+
+import org.informatics.PaperSize;
+import org.informatics.PaperType;
+
+public class Poster extends Edition {
+    public Poster(String title, int numberOfPages, boolean isColor, PaperType paperType, PaperSize paperSize) {
+        super(title, numberOfPages, isColor, paperType, paperSize);
+    }
+
+    public String getEditionType() {
+        return "Poster";
+    }
+}
