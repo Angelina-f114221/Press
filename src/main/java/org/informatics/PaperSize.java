638bc4cb855ebc11be6432290baa322034ff92b8
Angelina <angelina.ivaylova.ivanova@gmail.com>
2025-05-25 18:05:25 +0300

    Update PaperSize

--- src/main/java/org/informatics/PaperSize.java
+++ src/main/java/org/informatics/PaperSize.java
@@ -1,28 +1,19 @@
 package org.informatics;
 
-import java.math.BigDecimal;
-
 public enum PaperSize {
-    A5, A4, A3, A2, A1;
+    A5(1.0),
+    A4(1.5),
+    A3(2.0),
+    A2(3.0),
+    A1(4.0);
 
-    public BigDecimal getSizeMultiplier(BigDecimal increasePercent) {
-        int steps = getStepsFromA5();
-        BigDecimal multiplier = BigDecimal.valueOf(1.0);
-        for (int i = 0; i < steps; i++) {
-            multiplier = multiplier.multiply(increasePercent.add(BigDecimal.valueOf(1)));
-        }
-        return multiplier;
+    private final double multiplier;
+
+    PaperSize(double multiplier) {
+        this.multiplier = multiplier;
     }
 
-
-    private int getStepsFromA5() {
-        switch (this) {
-            case A5: return 0;
-            case A4: return 1;
-            case A3: return 2;
-            case A2: return 3;
-            case A1: return 4;
-            default: return 0;
-        }
+    public double getMultiplier() {
+        return multiplier;
     }
 }
